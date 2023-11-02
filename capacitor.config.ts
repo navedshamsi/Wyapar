088a881bf0c572c9b99885993097fb45a392b4c2
unknown <navedshamsi.cal@gmail.com>
2023-11-02 19:12:26 +0530

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,12 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'Wyapar',
+  webDir: 'www',
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;
