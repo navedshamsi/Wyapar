0052a2da7303457eecd0505cbe69e712486965cb
unknown <navedshamsi.cal@gmail.com>
2023-11-04 20:38:29 +0530

    home page created

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -3,17 +3,20 @@ import { CommonModule } from '@angular/common';
 import { IonicModule } from '@ionic/angular';
 import { FormsModule } from '@angular/forms';
 import { HomePage } from './home.page';
-
 import { HomePageRoutingModule } from './home-routing.module';
 
+// var data=[{1:"DESCRIPTION",2:"MODEL",3:"QUNATITY",4:"PRICE",5:"SUM"},{1:1,2:2,3:3,4:4,5:5},{1:1,2:2,3:3,4:4,5:5},
+//   {1:1,2:2,3:3,4:4,5:5},{1:1,2:2,3:3,4:4,5:5},{1:1,2:2,3:3,4:4,5:5},{1:1,2:2,3:3,4:4,5:5},{1:1,2:2,3:3,4:4,5:5},{1:1,2:2,3:3,4:4,5:5}
+//  ]
 
 @NgModule({
   imports: [
     CommonModule,
     FormsModule,
     IonicModule,
-    HomePageRoutingModule
+    HomePageRoutingModule,
   ],
   declarations: [HomePage]
 })
-export class HomePageModule {}
+export class HomePageModule {
+}
