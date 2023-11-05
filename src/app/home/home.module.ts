7979110c80f36acb19c06f7680ceeded42d2064b
unknown <navedshamsi.cal@gmail.com>
2023-11-05 10:06:56 +0530

    home page made dynamic

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -5,9 +5,7 @@ import { FormsModule } from '@angular/forms';
 import { HomePage } from './home.page';
 import { HomePageRoutingModule } from './home-routing.module';
 
-// var data=[{1:"DESCRIPTION",2:"MODEL",3:"QUNATITY",4:"PRICE",5:"SUM"},{1:1,2:2,3:3,4:4,5:5},{1:1,2:2,3:3,4:4,5:5},
-//   {1:1,2:2,3:3,4:4,5:5},{1:1,2:2,3:3,4:4,5:5},{1:1,2:2,3:3,4:4,5:5},{1:1,2:2,3:3,4:4,5:5},{1:1,2:2,3:3,4:4,5:5},{1:1,2:2,3:3,4:4,5:5}
-//  ]
+
 
 @NgModule({
   imports: [
--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
-export class HomePage {
-
+export class HomePage implements OnInit{
+ data=[{a:"DESCRIPTION",b:"MODEL",c:"QUNATITY",d:"PRICE",e:"SUM"},
+ {a:1,b:2,c:3,d:4,e:5},
+ {a:1,b:2,c:3,d:4,e:5},
+ {a:1,b:2,c:3,d:4,e:5},
+ {a:1,b:2,c:3,d:4,e:5},
+ {a:1,b:2,c:3,d:4,e:5},
+ {a:1,b:2,c:3,d:4,e:5},
+ {a:1,b:2,c:3,d:4,e:5},
+ {a:1,b:2,c:3,d:4,e:5}
+  ]
   constructor() {}
-
+  ngOnInit(): void {
+  }
 }
