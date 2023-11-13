dea1df7454550089d298fbbb88e99c840d6dbdde
unknown <navedshamsi.cal@gmail.com>
2023-11-13 18:56:38 +0530

    added sale

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -10,7 +10,11 @@ const routes: Routes = [
     path: '',
     redirectTo: 'home',
     pathMatch: 'full'
+  },  {
+    path: 'sale',
+    loadChildren: () => import('./sale/sale.module').then( m => m.SalePageModule)
   },
+
 ];
 
 @NgModule({
